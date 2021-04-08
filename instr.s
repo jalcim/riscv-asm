a3d0c5e112236cbdfd05647f03d07543b6b33c7f
ghost <alcim.dev@gmail.com>
2021-04-08 03:54:45 +0200

    man

--- instr.s
+++ instr.s
@@ -1,67 +1,55 @@
 	.global _start
 
 _start:
-	addi t1, t1, 0x7ff
-	slti t3, t2, 0x7ff
-	sltiu t3, t2, 0x7ff
+	addi  t0, t1, 0x7ff
+	slti  t0, t1, 0x7ff
+	sltiu t0, t1, 0x7ff
+	andi t0, t1, 0x7ff
+	ori  t0, t1, 0x7ff
+	xori t0, t1, 0x7ff
 
-	andi t1, t2, 0x7ff
-	ori t1, t2, 0x7ff
-	xori t1, t2, 0x7ff
+	slli t0, t1, 0x7
+	srli t0, t1, 0x7
+	srai t0, t1, 0x7
 
-	slli t1, t2, 0x7
-	srli t1, t2, 0x7
-	srai t1, t2, 0x7
+	lui   t0, 0xbeef
+	auipc t0, 0xbeef
 
-	lui t1, 0xbeef
-	auipc t1, 0xbeef
+	add  t0, t1, t2
+	slt  t0, t1, t2
+	sltu t0, t1, t2
 
-	add t2, t4, t5
-	slt t2, t4, t5
-	sltu t2, t4, t5
+	and t0, t1, t2
+	or  t0, t1, t2
+	xor t0, t1, t2
 
-	and t2, t4, t5
-	or t2, t4, t5
-	xor t2, t4, t5
+	sll t0, t1, t2
+	srl t0, t1, t2
 
-	sll t2, t4, t5
-	srl t2, t4, t5
+	sub t0, t1, t2
+	sra t0, t1, t2
 
-	sub t2, t4, t5
-	sra t2, t4, t5
-
-	jal ra, finish
+	jal  ra, finish
 	jalr ra, zero, 0x4
-
-
-
-
-
-
 	
-	beq t1, t1, finish
-	bne t1, t2, finish
+	beq t0, t1, finish
+	bne t0, t1, finish
 
-	blt t1, t2, finish
-	bltu t1, t2, finish
+	blt  t0, t1, finish
+	bltu t0, t1, finish
 
-	bge t1, t2, finish
-	bgeu t1, t2, finish
-
-
-
-
-
+	bge  t0, t1, finish
+	bgeu t0, t1, finish
 	
-	lw t1, 7(t6)
-	lh t1, 7(t6)
-	lhu t1, 7(t6)
-	lb t1, 7(t6)
-	lbu t1, 7(t6)
+	lw  t0, 7(t1)
+	lh  t0, 7(t1)
+	lhu t0, 7(t1)
+	lb  t0, 7(t1)
+	lbu t0, 7(t1)
 
-	sw t1, 7(t6)
-	sh t1, 7(t6)
-	sb t1, 7(t6)
+	sw t0, 7(t1)
+	sh t0, 7(t1)
+	sb t0, 7(t1)
 	
 	
 
